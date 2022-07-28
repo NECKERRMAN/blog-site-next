674bff52fc5a3e369bcdb100bee8386f968f9dd2
NECKERRMAN <robin.deneckere@gmail.com>
2022-07-28 22:06:36 +0200

    :construction: Updated Header

---
+++ src/components/App/Header.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { MainRoutes } from '../../core/routing';
+
+const Header = () => {
+    return (
+        <header className="flex justify-between p-5 max-w-7xl mx-auto">
+            <div className="flex items-center space-x-5">
+                <Link href={MainRoutes.Index}>
+                    <img
+                        alt="logo"
+                        src="https://www.seekpng.com/png/full/816-8163712_mountain-range-euclidean-vector-torres-del-paine-vector.png"
+                        className="w-44 cursor-pointer object-contain"
+                    />
+                </Link>
+                <div className="hidden md:inline-flex items-center space-x-5">
+                    <p>About</p>
+                    <p>Contact</p>
+                    <p className="text-white bg-blue-600 px-4 py-1 rounded-full">
+                        Follow
+                    </p>
+                </div>
+            </div>
+            <div className="flex items-center space-x-5 text-blue-600">
+                <p>Sign In</p>
+                <p className="border px-4 py-1 rounded-full border-blue-600">
+                    Get Started
+                </p>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Header from '../components/Header'
+import Header from '../components/App/Header'
 
 const Home: NextPage = () => {
   return (
@@ -9,8 +9,7 @@ const Home: NextPage = () => {
         <title>Coding Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <Header />
-      <h1>Deneckere Blog</h1>
+      <Header />
     </div>
   )
 }
