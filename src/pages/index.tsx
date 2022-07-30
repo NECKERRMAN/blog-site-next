7a9aceba7ce5592a033e0a6de128f99920e0a6e3
NECKERRMAN <robin.deneckere@gmail.com>
2022-07-30 15:26:29 +0200

    :sparkles: Added typings, sanity config file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,14 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '@/components/App/Header'
 import Hero from '@/components/App/Hero'
+import { sanityClient, urlFor } from '../../sanity'
+import { Post } from 'typings'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post]
+}
+
+export default function Home({ posts }: Props) {
   return (
     <div className="">
       <Head>
@@ -16,4 +22,21 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `
+  *[_type == "post"]{
+    _id,
+    title,
+    author -> {
+    name,
+    image
+    }, 
+   description,
+   mainImage,
+   slug
+  }`;
+
+  const posts = await sanityClient.fetch(query)
+
+
+}
