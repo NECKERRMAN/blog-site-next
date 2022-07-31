ba6539c18292210246ed0b50aaba6ca0ff067d21
NECKERRMAN <robin.deneckere@gmail.com>
2022-07-31 17:26:47 +0200

    :sparkles: Created utils dir, added formatDate

---
+++ src/core/utils/formatDate.ts
@@ -0,0 +1,8 @@
+const formatDate = (dateString: string) => {
+  const fullDate = new Date(dateString);
+  let month= ["January","February","March","April","May","June","July", "August","September","October","November","December"];
+  const readable = `${fullDate.getDate()} ${month[fullDate.getMonth()]} ${fullDate.getFullYear()} - ${fullDate.getHours()}:${fullDate.getMinutes()}`
+  return readable;
+}
+
+export default formatDate
